7c43d44b731d667a1ac72c35e8891abe7620b710
jishnuvadakara <jishnuv750@gmail.com>
2024-08-18 00:11:43 +0530

    feat:add new filed in user model

--- user-service/src/infrastructure/database/monogoDB/models/userModel.ts
+++ user-service/src/infrastructure/database/monogoDB/models/userModel.ts
@@ -28,6 +28,11 @@ const userSchema = new Schema(
           "https://i.pinimg.com/originals/61/f7/5e/61f75ea9a680def2ed1c6929fe75aeee.jpg ",
       },
     },
+    isVefified:{
+      type:Boolean,
+      default:false
+
+    },
     isBlocked: {
       type: Boolean,
       default: false,
