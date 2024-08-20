8697aad6531217812f69cb50cdcd12b12f518ab2
jishnuvadakara <jishnuv750@gmail.com>
2024-08-20 16:27:54 +0530

    feat:add new function for update user password

--- auth-service/src/application/interface/IRespositories.ts
+++ auth-service/src/application/interface/IRespositories.ts
@@ -4,6 +4,7 @@ import { IOtp } from "@/domain/entities";
 export interface IRepositories {
   findUserByEmail: (email: string) => Promise<UserEntities | null>;
   createUser: (data: UserEntities) => Promise<UserEntities | null>;
- verifyOtp:(email:string,otp:string)=>Promise<UserEntities|null>
+  verifyOtp: (email: string, otp: string) => Promise<UserEntities | null>;
   findUserById: (id: string) => Promise<UserEntities | null>;
+  updatePassword:(email:string,password:string)=>Promise<UserEntities|null>
 }
