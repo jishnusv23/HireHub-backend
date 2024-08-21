5d8d435c1a345eba067e542888580547792bdee9
jishnuvadakara <jishnuv750@gmail.com>
2024-08-21 20:56:43 +0530

    feat:add rolebased access

--- user-service/src/infrastructure/routes/index.ts
+++ user-service/src/infrastructure/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { IDependancies } from "../../application/interface/IDependancies";
 import { controller } from "../../presentation/controller/";
-import {jwtMiddleware} from 'hirehub-middleware-version'
+import {jwtMiddleware,roleMiddleware} from 'hirehub-middleware-version'
 import { requireAdmin } from "../../_lib/common/middleWare/requireAdmin";
 // import { jwtMiddleware } from "jwt-middleware-learnup";
 
@@ -10,7 +10,9 @@ export const routes = (dependancies: IDependancies) => {
   const router = Router();
 
   router.route("/getUser").get(jwtMiddleware, getUser);
-  router.route("/get-all-interviewee").get(jwtMiddleware,requireAdmin,getAllInterviewee)
+  router
+    .route("/get-all-interviewee")
+    .get(jwtMiddleware, roleMiddleware(['admin']), getAllInterviewee);
   router.route("/profile-status-update").put(jwtMiddleware,requireAdmin,blockunblockUser)
 
   return router;
