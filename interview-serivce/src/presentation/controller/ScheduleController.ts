59b6d65dd81fbce2c92a3e225252dd810fd45de8
jishnuvadakara <jishnuv750@gmail.com>
2024-08-26 15:39:56 +0530

    update:checking fot the userdata fetching

--- interview-serivce/src/presentation/controller/ScheduleController.ts
+++ interview-serivce/src/presentation/controller/ScheduleController.ts
@@ -1,17 +1,23 @@
+import RabbitMQClient from "../../infrastructure/MQ/client";
 import { IDependancies } from "../../application/interface/IDependancies";
-import { Request,Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 
-export const SchedulInterviewController=(dependancies:IDependancies)=>{
-    const {useCases:{IScheduleUseCases}}=dependancies
+export const SchedulInterviewController = (dependancies: IDependancies) => {
+  const {
+    useCases: { IScheduleUseCases },
+  } = dependancies;
 
-    return async(req:Request,res:Response,next:NextFunction)=>{
-        try{
-            console.log(req.body)
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      console.log(req.body);
+      const client = await RabbitMQClient.getInstance();
+      const userId = await client.produce(req.body, "fetchUserId", "toUser");
+      console.log("🚀 ~ file: ScheduleController.ts:15 ~ return ~ userId:", userId)
 
-            const response=await IScheduleUseCases(dependancies).execute(req.body)
-            console.log("🚀 ~ file: ScheduleController.ts:12 ~ returnasync ~ response:", response)
-        }catch(error:any){
-            next(error)
-        }
+      // const response=await IScheduleUseCases(dependancies).execute(req.body)
+      // console.log("🚀 ~ file: ScheduleController.ts:12 ~ returnasync ~ response:", response)
+    } catch (error: any) {
+      next(error);
     }
-}
+  };
+};
