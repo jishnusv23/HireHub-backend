92966bed1a18e61b2aede5d26fb5dc865bfe3d4b
jishnuvadakara <jishnuv750@gmail.com>
2024-08-26 21:38:30 +0530

    fix:corrected the mistake

--- interview-serivce/src/infrastructure/database/monogoDB/repositories/createInterview.ts
+++ interview-serivce/src/infrastructure/database/monogoDB/repositories/createInterview.ts
@@ -6,23 +6,15 @@ export const createInterview = async (
   data: InterviewEntity
 ): Promise<InterviewEntity | any> => {
   try {
-    // if (!data._id) {
-    //   throw new Error("data._id is undefined");
-    // }
-    const saveData = await interview.create(data);
-
     const client = await RabbitMQClient.getInstance();
-    const updateRole = await client.produce(
-      saveData.interviewerId,
-      "updateRoleInterviewer",
-      "toUser"
-    );
-    if (updateRole) {
-      console.log("🚀 ~ file: createInterview.ts:21 ~ saveData:", saveData);
-      return saveData;
-    } else {
-      throw new Error("updateRole failed to return a valid value");
+    const updateRole = await client.produce(data.interviewerId,'updateRoleInterviewer','toUser')
+    console.log("🚀 ~ file: createInterview.ts:11 ~ updateRole:", updateRole)
+    if(updateRole){
+       const saveData = await interview.create(data);
+        return saveData;
     }
+    return null
+    
   } catch (error: any) {
     console.error("Error in createInterview:", error);
     throw new Error(error?.message);
