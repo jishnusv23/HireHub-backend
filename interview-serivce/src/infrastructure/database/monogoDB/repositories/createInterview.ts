4419ffcd3b9909bd8bb87550651aa36012442d0f
jishnuvadakara <jishnuv750@gmail.com>
2024-08-26 20:23:43 +0530

    feat:checking for the communicatioin

--- interview-serivce/src/infrastructure/database/monogoDB/repositories/createInterview.ts
+++ interview-serivce/src/infrastructure/database/monogoDB/repositories/createInterview.ts
@@ -1,16 +1,19 @@
 import { interview } from "../models";
 import { InterviewEntity } from "../../../../domain/entities";
+import RabbitMQClient from "@/infrastructure/MQ/client";
 
 export const createInterview = async (
   data: InterviewEntity
 ): Promise<InterviewEntity | any> => {
   try {
-    console.log(
-      "🚀 ~ file: createInterview.ts:5 ~ createInterview ~ data:",
-      data
-    );
-    const saveData= await interview.create(data)
-    return saveData
+    const client = await RabbitMQClient.getInstance();
+    const updateRole = await client.produce(data._id,'updateRoleInterviewer','toUser')
+    if(updateRole){
+       const saveData = await interview.create(data);
+        return saveData;
+    }
+    return null
+    
   } catch (error: any) {
     throw new Error(error?.message);
   }
