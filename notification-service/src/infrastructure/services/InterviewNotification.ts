b71d7c7a132447c2ba44fe0e469b6deb80439748
jishnuvadakara <jishnuv750@gmail.com>
2024-08-27 11:09:44 +0530

    fix:checking the email sending function

--- notification-service/src/infrastructure/services/InterviewNotification.ts
+++ notification-service/src/infrastructure/services/InterviewNotification.ts
@@ -1,13 +1,22 @@
 import { notifyInterview } from "../../util/nodeMailer";
 
 export const InterviewNotification = async (data: any) => {
-  try{
-    const participantEmail = data.participantEmail;
-  const response=await notifyInterview(data,participantEmail)
-  return response
-  }catch(error:any){
-    console.error('Something wrong in notification handle service folder',error);
-    throw new Error(error?.message)
-    
+  try {
+    const participants = data.participants;
+    //   const response=await notifyInterview(data,participantEmails)
+    //   return response
+   const response=  await Promise.all(
+       participants.map(async (email:string) => {
+        console.log(data,email)
+        //  await notifyInterview(data, email);
+       })
+     );
+     return response
+  } catch (error: any) {
+    console.error(
+      "Something wrong in notification handle service folder",
+      error
+    );
+    throw new Error(error?.message);
   }
 };
