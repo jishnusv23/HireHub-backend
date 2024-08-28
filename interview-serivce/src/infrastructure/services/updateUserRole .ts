5f098e533b82bacb2d30c27997b3ab895371b06d
jishnuvadakara <jishnuv750@gmail.com>
2024-08-28 18:40:15 +0530

    fix:corrected the mistake

--- interview-serivce/src/infrastructure/services/updateUserRole .ts
+++ interview-serivce/src/infrastructure/services/updateUserRole .ts
@@ -1,25 +1,31 @@
 import RabbitMQClient from "../MQ/client";
 
-export const updateUserRole = async (interviewerId: string): Promise<{ accessToken: string } | null> => {
+interface UpdateRoleResponse {
+  accessToken: string;
+ 
+}
+
+export const updateUserRole = async (
+  interviewerId: string
+): Promise<UpdateRoleResponse | null> => {
   try {
     const client = await RabbitMQClient.getInstance();
     console.log("Sending message to update user role:", interviewerId);
-    
-     const updateRole = await client.produce(
-       interviewerId,
-       "updateRoleInterviewer",
-       "toUser"
-     );
-     console.log("Message sent:", updateRole);
-    
-   
+
+    const updateRole = await client.produce(
+      interviewerId,
+      "updateRoleInterviewer",
+      "toUser"
+    ) as UpdateRoleResponse
+    console.log("Message sent:", updateRole);
+
     if (updateRole && updateRole.accessToken) {
       return { accessToken: updateRole.accessToken };
     }
-    
+
     return null;
   } catch (error) {
     console.error("Error updating user role:", error);
     throw new Error("Failed to update user role");
   }
-}
+};
