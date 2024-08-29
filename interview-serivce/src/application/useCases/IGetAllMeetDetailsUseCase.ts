c30f0cfe023ca02c78c4a5ed1b730c5d8b3119cd
jishnuvadakara <jishnuv750@gmail.com>
2024-08-29 10:05:15 +0530

    update:find the interviewer data

--- interview-serivce/src/application/useCases/IGetAllMeetDetailsUseCase.ts
+++ interview-serivce/src/application/useCases/IGetAllMeetDetailsUseCase.ts
@@ -1,12 +1,18 @@
-import { dependacies } from "@/_boot/dependencies";
 import { IDependancies } from "../interface/IDependancies";
 
-export const IGetAllMeetDetailsUseCases=(dependacies:IDependancies)=>{
-    const {repositories:{getAllMeetDetails}}=dependacies
+export const IGetAllMeetDetailsUseCases = (dependacies: IDependancies) => {
+  const {
+    repositories: { getAllMeetDetails },
+  } = dependacies;
 
-    return {
-        execute:async(page:number=1,limit:number=5,id:string,search?:string)=>{
-            return await getAllMeetDetails(page,limit,id,search)
-        }
-    }
-}
+  return {
+    execute: async (
+      page: number = 1,
+      limit: number = 5,
+      interviewerId?: string,
+      search?: string
+    ) => {
+      return await getAllMeetDetails(page, limit, interviewerId, search);
+    },
+  };
+};
--- interview-serivce/src/presentation/controller/getAlllDetailsMeet.ts
+++ interview-serivce/src/presentation/controller/getAlllDetailsMeet.ts
@@ -1,16 +1,34 @@
 import { NextFunction, Request, Response } from "express";
 import { IDependancies } from "../../application/interface/IDependancies";
 
-export const getAllMeetDetailsController=(dependacies:IDependancies)=>{
-    const {useCases:{IGetAllMeetDetailsUseCases}}=dependacies
-    return async(req:Request,res:Response,next:NextFunction)=>{
-        try{
-            console.log(req.user,'user token details')
+export const getAllMeetDetailsController = (dependacies: IDependancies) => {
+  const {
+    useCases: { IGetAllMeetDetailsUseCases },
+  } = dependacies;
 
-        console.log(req.query, "its getting in backend perfectly");
-        console.log("l");
-        }catch(errror:any){
-            next(errror)
-        }
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      console.log(req.user, "user token details"); 
+      console.log(req.query, "its getting in backend perfectly");
+
+      const interviewerId = req.user?._id as string; 
+      const { page, limit, search } = req.query;
+
+      const response = await IGetAllMeetDetailsUseCases(dependacies).execute(
+        Number(page),
+        Number(limit),
+        interviewerId,
+        search as string
+      );
+
+      console.log(
+        "🚀 ~ file: getAllMeetDetailsController.ts:13 ~ return async ~ response:",
+        response
+      );
+
+    
+    } catch (error: any) {
+      next(error);
     }
-}
+  };
+};
