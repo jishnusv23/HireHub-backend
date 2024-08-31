8e00bd08f435064bdc4f9d54154b0fed47f4d02d
jishnuvadakara <jishnuv750@gmail.com>
2024-08-31 10:46:53 +0530

    feat:return the response in cancel interview

--- interview-serivce/src/presentation/controller/cancelInterviewController.ts
+++ interview-serivce/src/presentation/controller/cancelInterviewController.ts
@@ -9,6 +9,14 @@ export const cancelInterveiwController=(dependancies:IDependancies)=>{
         try{
             console.log(req.body, "intervieweID");
 
+            const response=await ICancelInterviewUseCases(dependancies).execute(req.body)
+            console.log("🚀 ~ file: cancelInterviewController.ts:13 ~ returnasync ~ response:", response)
+            if(response){
+                return res.status(200).json({success:true,data:response,message:'Interview Canceled'})
+            }
+
+            return res.status(404).json({success:true,data:null,message:'Invalid Interviewer'})
+
         }catch(error:any){
             next(error)
         }
