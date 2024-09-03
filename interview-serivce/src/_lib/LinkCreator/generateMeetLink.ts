49ba969e9fa4721a69e2677367986b03d4f3f7ee
jishnuvadakara <jishnuv750@gmail.com>
2024-09-03 11:31:32 +0530

    update:changed the interview url

--- interview-serivce/src/_lib/LinkCreator/generateMeetLink.ts
+++ interview-serivce/src/_lib/LinkCreator/generateMeetLink.ts
@@ -3,6 +3,6 @@ export const generateMeetLink = (
 ): { meetingLink: string; uniqueId: string } => {
   const uniqueId = Math.random().toString(36).substring(2, 15);
 
-  const meetingLink = `http://localhost:5173/HireHubMeet/${uniqueId}`;
+  const meetingLink = `http://localhost:5173/Meet-HireHub/${uniqueId}`;
   return { uniqueId,meetingLink };
 };
