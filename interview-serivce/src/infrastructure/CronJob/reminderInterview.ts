c4a36e37199b5182fdd1d55587ea5441501fb9f7
jishnuvadakara <jishnuv750@gmail.com>
2024-09-08 23:15:19 +0530

    feat:update function

--- interview-serivce/src/infrastructure/CronJob/reminderInterview.ts
+++ interview-serivce/src/infrastructure/CronJob/reminderInterview.ts
@@ -4,8 +4,19 @@ import cron from "node-cron";
 export const reminderInteriview = async () => {
   try {
     const now = new Date();
-    const fifteenMinuteLater = new Date(now.getTime() + 15 * 60000);
+    // const fifteenMinuteLater = new Date(now.getTime() + 15 * 60000);
+    const todayStart = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate()
+    );
+    const todayEnd = new Date(todayStart);
+    todayEnd.setDate(todayEnd.getDate()+1);
 
-    const interivews = await interview.find({ startTime: {} });
+    const interivews = await interview.find({
+      date: { $gte: todayStart, $lt: todayEnd },
+      interviewStatus: "Scheduled",
+      reminded: { $ne: true },
+    });
   } catch (error: any) {}
 };
