0f5674129f3e6458db2f52d4465e36d1c05afd23
jishnuvadakara <jishnuv750@gmail.com>
2024-09-17 11:15:33 +0530

    fix:socket calling index file

--- interview-serivce/src/index.ts
+++ interview-serivce/src/index.ts
@@ -1,12 +1,15 @@
 import server from "./presentation/server";
 import database from "./_boot/database";
+import { socket } from "./_boot/socket";
 
 (async () => {
   try {
-    server.start()
-    await database()
-} catch (error: any) {
+    const httpServer = await server.start();
+    // server.start();
+    await database();
+    socket(httpServer)
+  } catch (error: any) {
     console.error(error?.message || "An error occurred");
     process.exit(1);
   }
-})()
+})();
