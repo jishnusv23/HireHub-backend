e9920329bccd262535d96e1c3bfae7d9315b3acf
jishnuvadakara <jishnuv750@gmail.com>
2024-09-26 13:20:18 +0530

    fix:connected the usecases injection

--- interview-serivce/src/application/useCases/IAddquestionUseCases.ts
+++ interview-serivce/src/application/useCases/IAddquestionUseCases.ts
@@ -1,9 +1,19 @@
+import { TechQuestionEntities } from "@/domain/entities";
 import { IDependancies } from "../interface/IDependancies";
 
 export const IAddquestionUseCases=(dependancies:IDependancies)=>{
-    const {}=dependancies
+    const {repositories:{AddQuestions}}=dependancies
 
     return {
+        execute:async(data:TechQuestionEntities)=>{
+            try{
+                return await AddQuestions(data)
+
+            }catch(error:any){
+                 console.error("Someting wrong in IAddquestionUseCases", error);
+                 throw new Error(error?.message);
+            }
+        }
         
     }
 }
