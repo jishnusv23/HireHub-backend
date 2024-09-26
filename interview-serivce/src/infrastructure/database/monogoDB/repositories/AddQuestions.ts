dd1bb029a3aaaae050fee676c4adae8ad5608139
jishnuvadakara <jishnuv750@gmail.com>
2024-09-26 13:36:57 +0530

    fix:correcting the injection bugs

--- interview-serivce/src/infrastructure/database/monogoDB/repositories/AddQuestions.ts
+++ interview-serivce/src/infrastructure/database/monogoDB/repositories/AddQuestions.ts
@@ -1,15 +1,16 @@
 import { TechQuestion } from "../models";
 import { TechQuestionEntities } from "../../../../domain/entities";
 
-export const AddQuestions=async(data:TechQuestionEntities):Promise<TechQuestionEntities|null>=>{
-    try{
-        console.log(data,'crating for this data')
-        const saveData=await TechQuestion.create(data)
-        return saveData
-
-
-    }catch(error:any){
-        console.error("Error in AddTechQuestion:", error);
-        throw new Error(error?.message);
-    }
-}
+export const AddQuestions = async (
+  data: TechQuestionEntities
+): Promise<TechQuestionEntities | null> => {
+  try {
+    console.log(data, "crating for this data");
+    const saveData = await TechQuestion.create(data);
+    console.log("🚀 ~ file: AddQuestions.ts:10 ~ saveData:", saveData)
+    return saveData;
+  } catch (error: any) {
+    console.error("Error in AddTechQuestion:", error);
+    throw new Error(error?.message);
+  }
+};
