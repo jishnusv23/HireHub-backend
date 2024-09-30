9f09915a1579b47b8bce225da2d161e0c6be5c7c
jishnuvadakara <jishnuv750@gmail.com>
2024-09-30 22:18:53 +0530

    feat:added new function for fetching content

---
+++ user-service/src/application/useCases/fetchAllContentUseCases.ts
@@ -0,0 +1,11 @@
+import { IDependancies } from "../interface/IDependancies";
+
+export const fetchAllContentUseCases=(dependancies:IDependancies)=>{
+    const {}=dependancies
+
+    return {
+        execute:async(page:number=1,limit:number=8)=>{
+            
+        }
+    }
+}
---
+++ user-service/src/infrastructure/database/monogoDB/repositories/fetchAllContent.ts
@@ -0,0 +1,23 @@
+import { contentM } from "../models";
+
+export const fetchAllContent=async(page:number=1,limit:number=8)=>{
+  try{
+    let skipNo = (page - 1) * limit;
+    const query: any = { AdminAccept:true }; 
+    const totalContents=await contentM.countDocuments(query)
+     const data = await contentM.find(query)
+       .sort({ createdAt: -1 })
+       .skip(skipNo)
+       .limit(limit);
+     console.log("🚀 ~ file: getAllInterviewee.ts:20 ~ data:", data);
+     return {
+        data,
+        totalPages:Math.ceil(totalContents/limit),
+        currentPages:page
+     }
+  }catch(error:any){
+    console.error('Something wrong in fetchAllContent',error);
+    throw new Error(error?.message)
+    
+  }
+}
