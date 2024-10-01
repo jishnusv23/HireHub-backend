f1428535f21c3f7bfa82269773387b821208c0c2
jishnuvadakara <jishnuv750@gmail.com>
2024-10-01 16:17:04 +0530

    fix:added the response

--- user-service/src/presentation/controller/getAllContentRequestController.ts
+++ user-service/src/presentation/controller/getAllContentRequestController.ts
@@ -6,7 +6,39 @@ export const getAllContentRequestController=(dependancies:IDependancies)=>{
 
     return async(req:Request,res:Response,next:NextFunction)=>{
         try{
-            console.log(req.query,'geting the query here .>>>>>>>>>>>>>>>>>>>>>')
+            const page = req.query.page
+              ? parseInt(req.query.page as string, 10)
+              : undefined;
+
+            const limit = req.query.limit
+              ? parseInt(req.query.limit as string, 10)
+              : undefined;
+
+            //*page!!
+            if (page !== undefined && isNaN(page)) {
+              return res
+                .status(400)
+                .json({ success: false, message: "Invalid Page Number" });
+            }
+
+            //* limiit!!
+            if (limit !== undefined && isNaN(limit)) {
+              return res
+                .status(400)
+                .json({ success: false, message: "Invalid limit Number" });
+            }
+            const response = await getallContentRequestUseCases(
+              dependancies
+            ).execute(page, limit);
+            console.log(
+              "🚀 ~ file: getAll.ts:34 ~ return ~ response:",
+              response
+            );
+            res.status(201).json({
+              success: true,
+              message: "Getting All requestContent",
+              data: response,
+            });
 
         }catch(error:any){
             next(error)
