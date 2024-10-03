2fee3ea14748507cff8c698abc8b6acfd5f24b7a
jishnuvadakara <jishnuv750@gmail.com>
2024-10-03 13:25:48 +0530

    fix:added the return response

--- interview-serivce/src/presentation/controller/adminFetchAllInterivewesController.ts
+++ interview-serivce/src/presentation/controller/adminFetchAllInterivewesController.ts
@@ -6,6 +6,23 @@ export const AdminFetchInterivewesController=(dependancies:IDependancies)=>{
     return async(req:Request,res:Response,next:NextFunction)=>{
         try{
             console.log(req.query,'geting the query successfully------------------------------------')
+             const { page, limit } = req.query;
+             const response = await IAdminFetchInterivewesUseCases(
+               dependancies
+             ).execute(Number(page), Number(limit));
+              if (response) {
+                return res
+                  .status(200)
+                  .json({
+                    success: true,
+                    data: response,
+                    message: "Fetching all data successfully",
+                  });
+              } else {
+                return res
+                  .status(400)
+                  .json({ success: true, data: null, message: "invalid query" });
+              }
         }catch(error:any){
             next(error)
         }
