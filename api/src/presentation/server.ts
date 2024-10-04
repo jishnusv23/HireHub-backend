bf3de383d9a2b97f94cb317789b982c0c2966a4d
jishnuvadakara <jishnuv750@gmail.com>
2024-10-04 09:52:36 +0530

    update:router config attach to router folder

--- api/src/presentation/server.ts
+++ api/src/presentation/server.ts
@@ -2,22 +2,27 @@ import express, { Application } from "express";
 import cookieParser from "cookie-parser";
 import { config } from "dotenv";
 import cors from "cors";
-import { corsOptions } from "../_boot/config";
 import { setupRoutes } from "../routes";
 
 config();
-const app: Application = express();
 
+const app: Application = express();
 const PORT = Number(process.env.PORT) || 2001;
+
+const corsOption = {
+  origin: String(process.env.FRONT_END_URL),
+  methods: "GET,POST,PUT,DELETE,HEAD",
+  credentials: true,
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(cors(corsOption));
 
+setupRoutes(app);
 
 const start = () => {
-  setupRoutes(app);
   app.listen(PORT, () => {
     console.log(`The ${process.env.SERVICE} is listening on port ${PORT}`);
   });
