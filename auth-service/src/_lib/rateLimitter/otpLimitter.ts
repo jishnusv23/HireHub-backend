60b8dc60528a305e9e65642465ac8c522020ac9a
jishnuvadakara <jishnuv750@gmail.com>
2024-10-04 12:06:22 +0530

    update:updated error handler

--- auth-service/src/_lib/rateLimitter/otpLimitter.ts
+++ auth-service/src/_lib/rateLimitter/otpLimitter.ts
@@ -1,12 +1,13 @@
 import rateLimit from "express-rate-limit";
 import { Request, Response } from "express";
+import { HttpStatusCode } from "../http/statusCode/HttpStatusCode";
 
 export const otpLimitter = rateLimit({
   windowMs: 5 * 60 * 100,
   max: 5,
   handler: (req: Request, res: Response) => {
     res
-      .status(429)
+      .status(HttpStatusCode.TOO_MANY_REQUESTS)
       .json({
         error: "Too many OTP verification attempts. Please try again later",
       });
