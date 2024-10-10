1f1cbc24f7bb2bc57f7b31751012fa6f325e36bf
jishnuvadakara <jishnuv750@gmail.com>
2024-10-10 19:24:16 +0530

    fix :added the cloud url

--- auth-service/src/infrastructure/rabbitmq/rabbitmq.config.ts
+++ auth-service/src/infrastructure/rabbitmq/rabbitmq.config.ts
@@ -1,7 +1,9 @@
 
 export default {
   rabbitMQ: {
-    url: process.env.RABBITMQ_URL || "amqp://guest:guest@localhost:5672",
+    url:
+      process.env.RABBITMQ_URL ||
+      "amqps://mskssqum:VTeOg6PCkmN_LJ1w6-wVbCewR3iBU-iP@ostrich.lmq.cloudamqp.com/mskssqum",
   },
   queues: {
     authQueue: "authQueue",
