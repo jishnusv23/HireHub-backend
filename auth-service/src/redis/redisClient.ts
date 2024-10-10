ea0f21d608c3baccf05c1d4c98697359940481ed
jishnuvadakara <jishnuv750@gmail.com>
2024-10-10 18:45:53 +0530

    update:rabbitmq cloud url and redis

--- auth-service/src/redis/redisClient.ts
+++ auth-service/src/redis/redisClient.ts
@@ -1,7 +1,11 @@
-import {Redis} from 'ioredis'
+import { Redis } from "ioredis";
+const redisURL = "rediss://red-cs3sj30gph6c73c7cbcg:DUcIdc3zCOpygCvfGYNa8Si2dW1AKTwW@oregon-redis.render.com:6379";
+export const Client = new Redis(redisURL);
 
-export const Client=new Redis()
+Client.on("connect", () => {
+  console.log("💡redis connected successfully.....");
+});
 
-Client.on('connect',()=>{
-    console.log('💡redis connected successfully.....')
-})
+Client.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
