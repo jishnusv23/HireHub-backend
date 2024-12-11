9d1c0fbdcbaa67c2e88cc8de94247fdeae741ce1
@jishnuv750 <jishnuv750@gmail.com>
2024-12-11 09:08:30 +0530

    add the new url

--- auth-service/src/infrastructure/rabbitmq/rabbitmq.config.ts
+++ auth-service/src/infrastructure/rabbitmq/rabbitmq.config.ts
@@ -2,9 +2,8 @@ import { config } from "dotenv";
 config()
 export default {
   rabbitMQ: {
-    url:
-      process.env.RABBITMQ_URL ||
-      "amqps://wjydjslj:IMXz7hq8aQBS6KeOUwA5hGJT7LvUu_qh@dog.lmq.cloudamqp.com/wjydjslj",
+    url:process.env.RABBITMQ_URL
+    
   },
   queues: {
     authQueue: "authQueue",
