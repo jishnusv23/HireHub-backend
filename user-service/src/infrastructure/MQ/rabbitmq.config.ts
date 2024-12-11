70bdc2999b7d61f8d829ea7e92dae2cdbb75ad64
@jishnuv750 <jishnuv750@gmail.com>
2024-12-11 20:26:08 +0530

    changing the url parts

--- user-service/src/infrastructure/MQ/rabbitmq.config.ts
+++ user-service/src/infrastructure/MQ/rabbitmq.config.ts
@@ -2,7 +2,9 @@ import { config } from "dotenv";
 config();
 export default {
   rabbitMQ: {
-    url: process.env.RABBITMQ_URL,
+    url:
+      process.env.RABBITMQ_URL ||
+      "amqp://guest:guest@rabbitmq.default.svc.cluster.local:5672",
   },
   queues: {
     authQueue: "authQueue",
